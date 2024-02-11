3ea69f9f7ca19d454ba7a411235ad6393b492441
SellSoft-collab <dfognini@yahoo.com>
2024-02-11 04:46:47 -0500

    Icon and orgization

--- Src/app.js
+++ Src/app.js
@@ -15,25 +15,25 @@ join(__dirname, "./converterWorker.js");
 
 console.log(
   chalk.bgMagenta.bold.cyan(`
-**************************************************************************************************************************
-*                                                                                                                        *
-*   MM""""""""'M M""""""""'M    MM'"""""'MM                                 MMP"""""""MM                dP oo            *
-*   MM  mmmmmmmM Mmmmmm   .M    M' .mmm. 'M                                 M' .mmmm  MM                88               *
-*   M'      MMMM MMMMP  .MMM    M  MMMMMMMM .d8888b. 88d8b.d8b. .d8888b.    M         'M dP    dP .d888b88 dP .d8888b.   *
-*   MM  MMMMMMMM MMP  .MMMMM    M  MMM   'M 88'  '88 88''88''88 88ooood8    M  MMMMM  MM 88    88 88'  '88 88 88'  '88   *
-*   MM  MMMMMMMM M' .MMMMMMM    M. 'MMM' .M 88.  .88 88  88  88 88.  ...    M  MMMMM  MM 88.  .88 88.  .88 88 88.  .88   *
-*   MM        .M M         M    MM.     .MM '88888P8 dP  dP  dP '88888P'    M  MMMMM  MM '88888P' '88888P8 dP '88888P'   *
-*   MMMMMMMMMMMM MMMMMMMMMMM    MMMMMMMMMMM                                 MMMMMMMMMMMM                                 *
-*                                                                                                                        *
-*   MM'""""'YMM                                                dP                                                        *
-*   M' .mmm. 'M                                                88                                                        *
-*   M  MMMMMooM .d8888b. 88d888b. dP   .dP .d8888b. 88d888b. d8888P .d8888b. 88d888b.                                    *
-*   M  MMMMMMMM 88'  '88 88'  '88 88   d8' 88ooood8 88'  '88   88   88ooood8 88'  '88                                    *
-*   M. 'MMM' .M 88.  .88 88    88 88 .88'  88.  ... 88         88   88.  ... 88                                          *
-*   MM.     .dM '88888P' dP    dP 8888P'   '88888P' dP         dP   '88888P' dP                                          *
-*   MMMMMMMMMMM                                                                                                          *
-*                                                                                                                        *
-**************************************************************************************************************************
+********************************************************************************************************************
+                                                                                                                    
+ MM""""""""'M M""""""""'M    MM'"""""'MM                                 MMP"""""""MM                dP oo          
+ MM  mmmmmmmM Mmmmmm   .M    M' .mmm. 'M                                 M' .mmmm  MM                88             
+ M'      MMMM MMMMP  .MMM    M  MMMMMMMM .d8888b. 88d8b.d8b. .d8888b.    M         'M dP    dP .d888b88 dP .d8888b. 
+ MM  MMMMMMMM MMP  .MMMMM    M  MMM   'M 88'  '88 88''88''88 88ooood8    M  MMMMM  MM 88    88 88'  '88 88 88'  '88 
+ MM  MMMMMMMM M' .MMMMMMM    M. 'MMM' .M 88.  .88 88  88  88 88.  ...    M  MMMMM  MM 88.  .88 88.  .88 88 88.  .88 
+ MM        .M M         M    MM.     .MM '88888P8 dP  dP  dP '88888P'    M  MMMMM  MM '88888P' '88888P8 dP '88888P' 
+ MMMMMMMMMMMM MMMMMMMMMMM    MMMMMMMMMMM                                 MMMMMMMMMMMM                               
+                                                                                                                    
+ MM'""""'YMM                                                dP                                                      
+ M' .mmm. 'M                                                88                                                      
+ M  MMMMMooM .d8888b. 88d888b. dP   .dP .d8888b. 88d888b. d8888P .d8888b. 88d888b.                                  
+ M  MMMMMMMM 88'  '88 88'  '88 88   d8' 88ooood8 88'  '88   88   88ooood8 88'  '88                                  
+ M. 'MMM' .M 88.  .88 88    88 88 .88'  88.  ... 88         88   88.  ... 88                                        
+ MM.     .dM '88888P' dP    dP 8888P'   '88888P' dP         dP   '88888P' dP                                        
+ MMMMMMMMMMM                                                                                                        
+                                                                                                                    
+********************************************************************************************************************
 `)
 );
 
---
+++ src/ico/icon.js
@@ -0,0 +1,39 @@
+const fs = require("fs");
+const path = require("path");
+const { load } = require("resedit/cjs");
+load().then((ResEdit) => {
+  // ResEdit will be the namespace object of resedit library
+  // (for example ResEdit.Data.IconFile is available)
+  function windowsPostBuild(output) {
+    const exe = ResEdit.NtExecutable.from(fs.readFileSync(output));
+    const res = ResEdit.NtExecutableResource.from(exe);
+    const iconFile = ResEdit.Data.IconFile.from(fs.readFileSync("icon.ico"));
+
+    ResEdit.Resource.IconGroupEntry.replaceIconsForResource(
+      res.entries,
+      1,
+      1033,
+      iconFile.icons.map((item) => item.data)
+    );
+
+    const vi = ResEdit.Resource.VersionInfo.fromEntries(res.entries)[0];
+
+    vi.setStringValues(
+      { lang: 1033, codepage: 1200 },
+      {
+        ProductName: "EZ Game Audio Conversion",
+        FileDescription: "Simple game audio conversion tool.",
+        CompanyName: "Fooney",
+        LegalCopyright: `GPL-3.0`,
+      }
+    );
+    vi.removeStringValue({ lang: 1033, codepage: 1200 }, "OriginalFilename");
+    vi.removeStringValue({ lang: 1033, codepage: 1200 }, "InternalName");
+    vi.setFileVersion(1, 0, 3, 1);
+    vi.setProductVersion(1, 0, 3, 1);
+    vi.outputToResourceEntries(res.entries);
+    res.outputResource(exe);
+    fs.writeFileSync(output, Buffer.from(exe.generate()));
+  }
+  windowsPostBuild("../../dist/EZ-Game-Audio.exe");
+});
