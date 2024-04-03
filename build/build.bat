9c63b8dd3d68538c4046666fb07f2fd7871153ba
SellSoft-collab <dfognini@yahoo.com>
2024-04-03 01:12:14 -0400

    update

--- build/build.bat
+++ build/build.bat
@@ -1,67 +1,25 @@
 
 @ECHO OFF
-@REM prompt $g
-@REM title %~n0
-@REM color 0F
-@REM set maxCount=100
-@REM set count=0
-@REM   rem --- Initialize.
-@REM   setlocal EnableDelayedExpansion
-@REM   set /A "MaxBalls = 30"
-@REM   set /A "MaxX     = 81"
-@REM   set /A "MaxY     = 81"
-@REM   set /A  "lines=MaxY+2","cols=MaxX+1"
-@REM   mode con lines=%lines% cols=%cols%
-@REM   for /L %%X in (1,1,%MaxX%)  do set "BlankRow= !BlankRow!"
-@REM   for /L %%B in (0,1,%MaxBalls%) do ( 
-@REM     set /A "Ball_%%B.c=%%B %% 9"
-@REM     set /A "Ball_%%B.x=!RANDOM! %% MaxX","Ball_%%B.i=(!RANDOM! %% 3) + 1"
-@REM     set /A "Ball_%%B.y=!RANDOM! %% MaxY","Ball_%%B.j=(!RANDOM! %% 2) + 1"
-@REM   )
-@REM :PERPETUAL_LOOP
-@REM PING localhost -n .1 >NUL
-@REM   rem --- Calculate changes.
-@REM   for /L %%B in (0,1,%MaxBalls%) do (
-@REM     set /A "Ball_%%B.x += Ball_%%B.i" 
-@REM     set /A "Ball_%%B.y += Ball_%%B.j"
-@REM     if /I !Ball_%%B.x! GEQ %MaxX% set /A "Ball_%%B.x = %MaxX%","Ball_%%B.i *= -1"
-@REM     if /I !Ball_%%B.x! LEQ 0      set /A "Ball_%%B.x = 0",     "Ball_%%B.i *= -1"
-@REM     if /I !Ball_%%B.y! GEQ %MaxY% set /A "Ball_%%B.y = %MaxY%","Ball_%%B.j *= -1"
-@REM     if /I !Ball_%%B.y! LEQ 0      set /A "Ball_%%B.y = 0",     "Ball_%%B.j *= -1"
-@REM   )
-@REM   rem --- Build Screen.
-@REM   for /L %%Y in (0,1,%MaxY%) do (
-@REM     set "Row_%%Y="
-@REM     for /L %%B in (0,1,%MaxBalls%) do (
-@REM       if /I !Ball_%%B.y! EQU %%Y (
-@REM         if "!Row_%%Y!"=="" set "Row_%%Y=%BlankRow%"
-@REM         set /A "x2 = Ball_%%B.x + 1"
-@REM         call set "Row_%%Y=%%Row_%%Y:~0,!Ball_%%B.x!%%!Ball_%%B.c!%%Row_%%Y:~!x2!%%"
-@REM       )
-@REM     )
-@REM   )
-@REM   rem --- Display Screen.
-@REM   cls
-@REM for /L %%Y in (0,1,%maxCount%) do (
-@REM     REM Check if the count has reached the maximum
-@REM     if !count! gtr %maxCount% (
-@REM         REM Do something if the count is greater than the maximum
-@REM         echo Maximum count reached.
-@REM     ) else (
-@REM         REM Do something else if the count is within the limit
-@REM         echo Current count: !count!
-@REM     )
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "$wshell = New-Object -ComObject wscript.shell;Sleep 1;$wshell.SendKeys('{F11}');"
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "$Host.UI.RawUI.WindowTitle = 'HACKED⋆༺𓆩☠︎︎𓆪༻⋆'"
 
-@REM     REM Increment the count
-@REM     set /a count+=1
-@REM )
-@REM goto :PERPETUAL_LOOP
-
-title EZ Builder for EZ Batch Audio Convertion
+@REM Change Color of text and background to something cooler. Redundant for reliability.
+echo $Host.UI.RawUI.BackgroundColor = 'DarkGreen' > "%USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+echo $Host.UI.RawUI.ForegroundColor = 'White' >> "%USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1"
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "$Host.UI.RawUI.BackgroundColor = 'DarkGreen'"
+powershell -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "$Host.UI.RawUI.ForegroundColor = 'White'"
+color 0A
 
 echo Starting build.bat...
+@rem This is a hack for pkg to find the node worker files.
+set PKG_ENV=packaging
+echo Building with pkg.js...
+goto :endof
+START pkg . -o .\dist\EZ-Game-Audio.exe --compress GZIP
+
+@REM Make HTML and PDF from markdown file then copy them over to dist
 echo Making HTML...
-start npx markdown-to-html --source README.md --output ./readmes/README.html pause
+start npx markdown-to-html --source README.md --output ./readmes/README.html
 if errorlevel 1 (
     echo Error: Markdown to HTML conversion failed.
     pause
@@ -70,20 +28,19 @@ if errorlevel 1 (
     echo Markdown to HTML conversion successful.
 )
 echo Done with HTML. Making PDF...
+echo Creating PDF
+
 start md-to-pdf ./README.md
-xcopy README.pdf .\readmes /v /y /q
+
 if exist README.pdf (
     echo PDF completed successfully.
-    xcopy README.pdf .\readmes /v /y /q
+    move README.pdf .\readmes /v /y /q
 ) else (
     echo Error: Conversion failed.
 )
 
-set PKG_ENV=packaging
 xcopy .\readmes .\dist /v /y /q
 xcopy .\ffmpeg-bin .\dist /v /y /q
-echo Building with pkg.js...
-START pkg . -o .\dist\EZ-Game-Audio.exe --compress GZIP
 
 :WAIT_LOOP
 if not exist ".\dist\EZ-Game-Audio.exe" (
@@ -91,18 +48,57 @@ if not exist ".\dist\EZ-Game-Audio.exe" (
     goto :WAIT_LOOP
 )
 echo pkg.js finished!
+if not exist ".\dist\README.pdf" (
+    timeout /t 1 >nul
+    goto :WAIT_LOOP
+)
+if not exist ".\dist\README.html" (
+    timeout /t 1 >nul
+    goto :WAIT_LOOP
+)
+
 echo resedit is generating icon and meta data...
 node src\ico\icon.js
 echo resedit finished!
 cd .\dist\
 echo Zipping...
-powershell -Command "Compress-Archive -Update -Path .\* -DestinationPath ..\build\EZ-Game-Audio-Conversion.zip -CompressionLevel Optimal"
+powershell -Command "Compress-Archive -Update -Path .\* -DestinationPath ..\release\EZ-Game-Audio-Conversion.zip -CompressionLevel Optimal"
 echo Zipped!
 echo Making .7z...
-"C:\Progra~1\7-Zip\7zG.exe" a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on ..\build\EZ-Game-Audio-Conversion.7z .\* || (
+"C:\Progra~1\7-Zip\7zG.exe" a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on ..\release\EZ-Game-Audio-Conversion.7z .\* || (
     echo .7z finished!
     echo 7-Zip not found or had an error. .7z will not be made
 )
-del /Q * >nul
+@REM del /Q * >nul
 echo Build done!
+
+:endof
+
+cd .\dist\
+cd ../build/frames
+
+:loop
+
+set /A "num_frames=13"
+set /A "frame_counter=1"
+
+:inner_loop
+if exist "frame%frame_counter%.txt" (
+    type "frame%frame_counter%.txt"
+) else (
+    echo Frame file not found: "frame%frame_counter%.txt"
+)
+
+pathping 127.0.0.1 -n -q 1 -p 90 >nul
+
+set /A "frame_counter+=1"
+
+set /A "frame_counter=frame_counter %% num_frames"
+if %frame_counter% equ 0 set "frame_counter=%num_frames%"
+
+cls
+
+goto inner_loop
+
 exit
+
