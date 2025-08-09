8bea59a0a57c073e00ecd13128b7b632a32c7858
SpaceFoon <dfognini@yahoo.com>
2025-08-09 00:31:06 -0400

    chore: remove obsolete Src directory after migration to src

--- src/exitProgramError.js
+++
@@ -1,7 +0,0 @@
-class ExitProgramError extends Error {
-  constructor(message = "EXIT_PROGRAM") {
-    super(message);
-    this.name = "ExitProgramError";
-  }
-}
-module.exports = ExitProgramError;
